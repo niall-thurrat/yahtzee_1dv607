1f40cdda428a56da6cdab1cd57a325e785e3bf80
niall-thurrat <amidst_art@hotmail.com>
2020-01-03 12:35:36 +0100

    else ifs changed to switch in Run(), GamerPlays() created

--- Yahtzee/controller/Application.cs
+++ Yahtzee/controller/Application.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using MainMenuInput = Yahtzee.view.UI.MainMenuInput;
+using GameMenuInput = Yahtzee.view.UI.GameMenuInput;
 
 namespace Yahtzee.controller
 {
@@ -23,55 +24,111 @@ namespace Yahtzee.controller
 
             var input = m_view.GetMainInput();
 
-            /// ///////////////////////////////////////// CHANGE TO SWITCH
-            if (input == MainMenuInput.Play)
+            switch (input)
             {
-                var players = m_view.GetPlayers();
-                m_game = new model.Game(players);
+                case MainMenuInput.Play:
+                    var players = m_view.GetPlayers();
+                    m_game = new model.Game(players);
+                    
+                    while (m_game.Status == "InProgress")
+                    {
+                        // computer players continue to play until it's a gamer's turn (true) or game ends (false)
+                        bool GamerToPlay = m_game.Play();
+
+                        // GAMER'S TURN
+                        if (GamerToPlay)
+                        {
+                            GamerPlays();
+                        }
+                        // GAME OVER
+                        else
+                        {
+                            m_game.SaveGame();
+                        }
+                    }
                 
-                while (m_game.Status == "InProgress")
-                {
-                    // computer players continue to play until it's a gamer's turn (true) or game ends (false)
-                    bool GamerToPlay = m_game.Play();
+                    return true;
 
-                    // GAMER'S TURN
-                    if (GamerToPlay)
+                case MainMenuInput.Continue:
+                    // get/deserialize saved json object
+                    // parse necesary info
+                    // initialize and instanciate game object with info
+
+                    return true;
+
+                case MainMenuInput.ViewPrevious:
+                    // menu to choose full details or short list
+                    // access data to display
+
+                    return true;
+
+                case MainMenuInput.Quit:
+                    // display some sort of thanks for playing message
+                    // thread.sleep
+                    // clear
+                    return false;
+
+                default:
+                    // throw new Exception("menu input not recognised");
+                    // thread.sleep 2 sec
+                    return true;
+            }
+        }
+
+        public bool GamerPlays()
+        {
+            m_view.DisplayGameMenu();
+
+            var input = m_view.GetGameInput();
+
+            switch (input)
+            {
+                case GameMenuInput.Roll:
+                    var players = m_view.GetPlayers();
+                    m_game = new model.Game(players);
+                    
+                    while (m_game.Status == "InProgress")
                     {
-                        m_view.DisplayGameMenu();
+                        // computer players continue to play until it's a gamer's turn (true) or game ends (false)
+                        bool GamerToPlay = m_game.Play();
+
+                        // GAMER'S TURN
+                        if (GamerToPlay)
+                        {
+                            GamerPlays();
+                        }
+                        // GAME OVER
+                        else
+                        {
+                            m_game.SaveGame();
+                        }
                     }
-                    // GAME OVER
-                    else
-                    {
-                        m_game.SaveGame();
-                    }
-                }
-            
-                return true;
-            }
-            else if (input == MainMenuInput.Continue)
-            {
-                // get/deserialize saved json object
-                // parse necesary info
-                // initialize and instanciate game object with info
+                
+                    return true;
 
-                return true;
-            }
-            else if (input == MainMenuInput.ViewPrevious)
-            {
-                // menu to choose full details or short list
-                // access data to display
+                case GameMenuInput.HoldDie1:
+                    // get/deserialize saved json object
+                    // parse necesary info
+                    // initialize and instanciate game object with info
 
-                return true;
-            }
-            else if (input == MainMenuInput.Quit)
-            {
-                // display some sort of thanks for playing message
-                // thread.sleep
-                // clear
-                return false;
-            }
+                    return true;
 
-            return true;
+                case GameMenuInput.ChooseCat:
+                    // menu to choose full details or short list
+                    // access data to display
+
+                    return true;
+
+                case GameMenuInput.Quit:
+                    // player asked if would like to save
+                    // handle if wants to save game
+                    return false;
+
+                default:
+                    // throw new Exception("menu input not recognised");
+                    // thread.sleep 2 sec
+                    return true;
+            }
         }
     }
 }
